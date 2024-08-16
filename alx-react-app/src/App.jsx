75419cc959883ea6305c0de198ab41dc7161ba91
kimwaki <moreenkim36@gmail.com>
2024-08-16 11:54:21 +0300

    create first page and import welcome component

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
