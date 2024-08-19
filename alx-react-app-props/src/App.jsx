cd174b7efe5d6ed27ba209107ed04c80f7ac4184
kimwaki <moreenkim36@gmail.com>
2024-08-19 12:01:08 +0300

    move files

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,28 +1,53 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import WelcomeMessage from "./components/WelcomeMessage.jsx";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent.jsx";
-import Footer from "./components/Footer.jsx";
-import UserProfile from "./components/UserProfile.jsx";
-
-
+import ProfilePage from './ProfilePage';
+import UserContext from './src/UserContext';
 function App() {
-  const [count, setCount] = useState(0);
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
 
   return (
-    <>
-      <div>
-        <WelcomeMessage />
-        <Header />
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
-        <MainContent/>
-        <Footer/>
-      </div>
-    </>
-  );
+    <UserContext.provider userData={userData}>
+    <ProfilePage userData={userData} />;
+
+    </UserContext.provider>
+    
+  )
+
 }
 
 export default App;
+
+
+
+
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import WelcomeMessage from "./components/WelcomeMessage.jsx";
+// import Header from "./components/Header";
+// import MainContent from "./components/MainContent.jsx";
+// import Footer from "./components/Footer.jsx";
+// import UserProfile from "./components/UserProfile.jsx";
+
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   return (
+
+//     <UserContext.provider userData={userData}>
+//     <ProfilePage userData={userData} />;
+
+//     </UserContext.provider>
+//     // <>
+//     //   <div>
+//     //     <WelcomeMessage />
+//     //     <Header />
+//     //     <UserProfile name="Alice" age="25" bio="Loves hiking and photography"/>
+//     //     <MainContent/>
+//     //     <Footer/>
+//     //   </div>
+//     // </>
+//   );
+// }
+
+// export default App;
