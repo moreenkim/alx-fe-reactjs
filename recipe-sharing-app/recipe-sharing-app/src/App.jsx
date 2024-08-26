2430d8591a60390911a2a6549cf7bf3832e9e4ac
kimwaki <moreenkim36@gmail.com>
2024-08-26 12:16:51 +0300

    create and implement the recipe list

---
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
+
+function App() {
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-96 p-6 rounded-lg shadow-md bg-white text-center">
+        <div className="mb-6">
+          <h1 className="text-2xl font-semibold">Task Tracker</h1>
+        </div>
+        <div className="mb-4">
+          <RecipeList />
+        </div>
+         <AddRecipeForm />
+      </div>
+    </div>
+  );
+}
+
+export default App
