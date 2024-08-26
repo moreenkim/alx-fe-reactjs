41435ea9f877fdec76e4a4b314121743d578c0d2
kimwaki <moreenkim36@gmail.com>
2024-08-26 13:12:04 +0300

    rename the file

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -7,7 +7,7 @@ import AddRecipeForm from './components/AddRecipeForm';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeDetails from './components/RecipeDetails';
 import EditRecipeForm from './components/EditRecipeForm';
-import FavoritesList from './components/FavouritesList';
+import FavoritesList from './components/FavoritesList';
 import RecommendationsList from './components/RecommendationsList'
 
 function App() {
