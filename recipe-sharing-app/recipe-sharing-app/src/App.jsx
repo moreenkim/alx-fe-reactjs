4d89bd721798f358dacfb1f52d430212b725c1d2
kimwaki <moreenkim36@gmail.com>
2024-08-26 12:17:21 +0300

    make changes to the name

--- recipe-sharing-app/recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/recipe-sharing-app/src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <div className="flex justify-center items-center h-screen">
       <div className="w-96 p-6 rounded-lg shadow-md bg-white text-center">
         <div className="mb-6">
-          <h1 className="text-2xl font-semibold">Task Tracker</h1>
+          <h1 className="text-2xl font-semibold">Recipe list</h1>
         </div>
         <div className="mb-4">
           <RecipeList />
