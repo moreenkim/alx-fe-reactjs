295599fedfa0660ac8848dc101e61906c9a5e33f
kimwaki <moreenkim36@gmail.com>
2024-08-29 17:02:34 +0300

    make changes required

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from "./components/Profile";
 import Login from './components/Login';
-import Blogs from "./components/Blogs";
+import BlogPost from "./components/BlogPost";
 import PostList from "./components/PostList";
 import PostDetails from "./components/PostDetails";
 import { AuthProvider } from "./components/Auth";
@@ -17,7 +17,7 @@ function App() {
           {/* Route to display the list of posts */}
           <Route path="/posts" element={<PostList />} />
           {/* Dynamic route to display individual post details */}
-          <Route path="/blog/:id" element={<Blogs />} />
+          <Route path="/blog/:id" element={<BlogPost />} />
 
           <Route path="/post/:postId" element={<PostDetails />} />
 
