4ae2991388f31d0ea1fbd9ad598908ee1f1da325
kimwaki <moreenkim36@gmail.com>
2024-09-05 11:51:39 +0300

    make styling changes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full w-24 h-24 sm:w-36 sm:h-36 mx-auto"
+          className="rounded-full sm:w-24 sm:h-24 sm:w-36 sm:h-36 mx-auto"
         />
         <h1 className="text-lg sm:text-xl text-blue-800 my-4">John Doe</h1>
         <p className="text-sm sm:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
