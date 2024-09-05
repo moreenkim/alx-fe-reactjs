77749b5ce11e932e497b790a3638e4ad5c96e4c8
kimwaki <moreenkim36@gmail.com>
2024-09-05 11:56:11 +0300

    make styling changes

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,10 +1,10 @@
 function UserProfile() {
     return (
-      <div className="user-profile hover:scale-110 transition-transform duration-300 ease-in-out bg-gray-100  md:text-xl md:max-w-sm md:p-8 sm:w-24 sm:h-24 md:w-36 md:h-36 sm:p-4p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
+      <div class="user-profile hover:scale-110 transition-transform duration-300 ease-in-out bg-gray-100  md:text-xl md:max-w-sm md:p-8 sm:w-24 sm:h-24 md:w-36 md:h-36 sm:p-4p-4 sm:p-8 max-w-xs sm:max-w-sm mx-auto my-20 rounded-lg shadow-lg">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full hover:scale-110 transition-transform duration-300 ease-in-out w-24 h-24 sm:w-36 sm:h-36 mx-auto"
+          class="rounded-full hover:scale-110 transition-transform duration-300 ease-in-out w-24 h-24 sm:w-36 sm:h-36 mx-auto"
         />
         <h1 className="text-lg sm:text-xl hover:text-blue-500 text-blue-800 my-4">John Doe</h1>
         <p className="text-sm hover:text-blue-500 sm:text-base text-gray-600">Developer at Example Co. Loves to write code and explore new technologies.</p>
